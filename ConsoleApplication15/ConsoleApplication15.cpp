ff5719a9ef37a12c2caa299c89ef514d7a175d10
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-20 19:56:56 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <array>
+#include <cmath>
+
+int main()
+{
+    std::array<int, 25> masyv;
+
+    int n;
+    std::cout << "Enter an integer: ";
+    std::cin >> n;
+
+    if (n < 25)
+    {
+        for (int i = 0; i < n; i++)
+            masyv[i] = std::pow(2, i);
+
+        std::cout << "Masyv filled:" << std::endl;
+        for (int i = 0; i < n; i++)
+        {
+            std::cout << "masyv[" << i << "] = " << masyv[i] << std::endl;
+        }
+    }
+    else
+        std::cout << "The number entered exceeds the allowed size of the array.";
+}
